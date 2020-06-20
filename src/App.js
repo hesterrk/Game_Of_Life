7ca68ff7d1ebee39bbe4c00446c68b01e4f8e63d
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-20 16:59:08 +0100

    ft: imported grid component into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Grid from './components/Grid'
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome To Conway's Game Of Life </h1>
+      <Grid />
       
     </div>
   );
