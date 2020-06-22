b1cd11903eab09cf23df0444cae68d4a9df89225
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-22 14:02:53 +0100

    added game rules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Grid from './components/Grid'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome To Conway's Game Of Life </h1>
       <Grid />
+      <About/>
       
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h3> The Rules of the Game: </h3>
+      <ol>
+        <li>
+          A live cell with fewer than two live neighbours dies, as if by
+          underpopulation
+        </li>
+        <li>
+          A live cell surrounded by two or three live neighbours lives on to the
+          next generation.
+        </li>
+        <li>
+          A dead cell with exactly three live neighbours becomes a live cell, as
+          if by reproduction
+        </li>
+        <li>
+          A live cell surrounded by more than three live neighbours dies, as if
+          by overpopulation.
+        </li>
+        <p></p>
+      </ol>
+    </div>
+  );
+}
+
+export default About;
