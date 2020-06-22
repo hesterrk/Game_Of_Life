67782881c5823dcdeb122d8913689447d35e252d
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-22 14:40:54 +0100

    set up controls component, added to render in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Grid from './components/Grid'
+import React from "react";
+import "./App.css";
+import Grid from "./components/Grid";
+import Controls from "./components/Controls";
 
 function App() {
   return (
     <div className="App">
       <h1> Welcome To Conway's Game Of Life </h1>
       <Grid />
-      
     </div>
   );
 }
---
+++ src/components/Controls.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+function Controls() {
+
+
+
+
+
+    return <div>
+
+    </div>
+
+}
+
+
+export default Controls
