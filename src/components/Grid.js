f3741c9b045f5c0dcc27c99b4efda4517131666c
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-22 12:52:25 +0100

    using immer's produce function to create a copy of our original state to update our state with so we dont mutate our original state, toggle each individual cell works, background color changes depending on whether its alive or dead

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import produce from "immer";
 
 function Grid() {
   // Creating data structure: an row array with nested column arrays
@@ -43,6 +44,15 @@ function Grid() {
                 width: 20,
                 height: 20,
                 background: grid[r_i][col_i] === 0 ? "white" : "black",
+              }}
+              onClick={() => {
+                //Toggling each cell in the grid's own state
+                //Using produce to prevent the mutation of the original grid state, instead we create a copy and change this copy in order to change and toggle the state
+                const toggleGrid = produce(grid, (copyState) => {
+                  //assigning our copy state to value of our original state, whether the cell is alive or dead
+                  copyState[r_i][col_i] = grid[r_i][col_i] === 0 ? 1 : 0;
+                });
+                setGrid(toggleGrid);
               }}
             />
           ))
