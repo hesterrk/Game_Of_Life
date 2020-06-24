9518ce9b94667c6b046239041dba7cc5bad88e3e
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-24 13:03:23 +0100

    added background color to root div

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Grid from './components/Grid'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{border: '2px solid green', width: '100vw', height: '100vh', animation: 'stretch', animationDuration: '2s', animationTimingFunction:'ease-out', animationDirection: 'alternate', animationDelay: '0s', backgroundColor: '#2C5364' }}>
       <h1> Welcome To Conway's Game Of Life </h1>
       <Grid />
       
