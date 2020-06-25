7675c0be544e873e2555f9d04f3501b6df860813
Hester Knight <hesterknight@Hesters-MBP.cust.communityfibre.co.uk>
2020-06-25 14:00:18 +0100

    added some box shadow style to the grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,7 +4,7 @@ import Cell from "./Cell";
 function Grid({ grid, toggleCellState, changeColour, getRandomColor }) {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
-      <div style={{ display: "grid", gridTemplateColumns: `repeat(25, 22px)` }}>
+      <div style={{ display: "grid", gridTemplateColumns: `repeat(25, 22px)`, boxShadow: '5px 10px 18px #171515' }}>
         {grid.map((row, r_i) =>
           row.map((col, col_i) => (
             // returns the individual cell component (box)
